f880303495f1611baa91b429b4f41a501625b82e
mauboro <trying.again999@outlook.com>
2023-05-01 17:24:40 -0300

    adding the main.py file

---
+++ main.py
@@ -0,0 +1,23 @@
+import os 
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+choice = input("Who would you like to talk to today? ")
+messages = [{"role": "system", "content": f"You're {choice}"}]
+
+while True:
+    content = input("User: ")
+    if content.lower() == "goodbye":
+        messages.append({"role": "user", "content": content})
+        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+        chat_response = completion.choices[0].message.content
+        print(f"{' '.join(messages[0]['content'].split()[1:])}: {chat_response}")
+        break
+
+    messages.append({"role": "user", "content": content})
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+    chat_response = completion.choices[0].message.content
+    print(f"{' '.join(messages[0]['content'].split()[1:])}: {chat_response}")
+
+
